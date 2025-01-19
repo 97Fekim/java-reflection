b225fe3e0c99f7fdfc02c6582051fda13347d5d4
97Fekim <rlatmdcjf9@gmail.com>
2025-01-19 19:25:44 +0900

    Field

---
+++ src/exercise6___Filed_JsonConverter/data/Actor.java
@@ -0,0 +1,11 @@
+package exercise6___Filed_JsonConverter.data;
+
+public class Actor {
+    private final String name;
+    private final String[] knownForMovies;
+
+    public Actor(String name, String[] knownForMovies) {
+        this.name = name;
+        this.knownForMovies = knownForMovies;
+    }
+}
